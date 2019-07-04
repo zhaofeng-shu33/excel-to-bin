63a8e2c279147d3263cfcfe5bfcd20ed1cba7790
zhaofeng-shu33 <616545998@qq.com>
2019-07-04 13:57:57 +0800

    add packager

--- utility.js
+++ utility.js
@@ -3,7 +3,6 @@ const path = require('path');
 const Buffer=require('buffer').Buffer;
 
 const Excel = require('exceljs');
-const deasync = require('deasync');
 // return True if convert successfully, otherwise return False
 function convert(file_full_path, num_of_sheet, column_num, array_len){
     //todo: check file extension
@@ -18,15 +17,9 @@ function convert(file_full_path, num_of_sheet, column_num, array_len){
             let file_name = path.basename(file_full_path);
             let write_file_name = file_name.split('.')[0] + '.bin';
             let write_file_full_path = path.join(path.dirname(file_full_path), write_file_name);
-            convert_inner(data, write_file_full_path, num_of_sheet, column_num, array_len);             
+            fs.writeFileSync(write_file_full_path, data);
             return true;
     });
 } 
 
-function convert_inner(data, file_full_path, num_of_sheet, column_num, array_len){
-    //todo: get converted_data
-    let converted_data = data;
-    
-    fs.writeFileSync(file_full_path, converted_data);
-}
 module.exports.convert = convert;
