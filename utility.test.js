d5c4ac3fa6145cdac5056b7d8e80e82b8d611da2
zhaofeng-shu33 <616545598@qq.com>
2019-07-05 10:21:26 +0800

    compare output result

--- utility.test.js
+++ utility.test.js
@@ -1,7 +1,11 @@
 const convert = require('./utility').convert;
+const fs = require('fs');
 test('test_convert', ()=>{
     convert('testdata/book.xlsx', 1, 1, 2).then(
         function(rv){
             expect(rv).toBe(true);
+            let converted_data = fs.readFileSync('testdata/book.bin');
+            expect(JSON.stringify(converted_data)).toBe(JSON.stringify(Buffer.from([1,3])));
         });
 });
+
