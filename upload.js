544e0858a8ae77bf5fc374b9b0799282f1e3ac39
zhaofeng-shu33 <616545598@qq.com>
2019-07-06 23:27:39 +0800

    add upload script

---
+++ upload.js
@@ -0,0 +1,31 @@
+/**
+ * 将 build/docs.json 上传到 oss
+ */
+const oss = require('ali-oss');
+const path = require('path');
+const fs = require('fs');
+const bucket = 'programmierung';
+const accessKeyId = process.env.AccessKeyId;
+const accessKeySecret = process.env.AccessKeySecret;
+
+const ossClient = oss({
+    bucket,
+    endpoint: 'oss-cn-shenzhen.aliyuncs.com',
+    accessKeyId,
+    accessKeySecret,
+    time: '120s',
+});
+var fromPath = '';
+fs.readdirSync('dist').map((name)=>{
+    if(name.search('.zip')>0){
+        fromPath = name;
+    }
+})
+fromPath = path.join('dist', fromPath);
+const toPath = path.join('excel-to-bin', 'excel-to-bin-' + process.platform + '.zip');
+
+console.log('start upload oss', fromPath, toPath);
+
+ossClient.put(toPath, fromPath).then((result) => {
+    console.log('upload success', result);
+});
